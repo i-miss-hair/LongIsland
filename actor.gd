55c58d8e6c66cc362ef2aa924d90a957710daad3
i-miss-hair <nydal.games@gmail.com>
2025-09-15 23:32:11 +0200

    Actor Health
    Added actor health

--- actor.gd
+++ actor.gd
@@ -1,6 +1,12 @@
 class_name Actor extends CharacterBody2D
 
 const SPEED = 90
+var hp: int = 1
 
 @onready var _sprite: Sprite2D = $Sprite2D
 @onready var _gun: Gun = $Gun
+
+func heal_hurt(n: int) -> void:
+	hp += n
+	if hp <=0:
+		queue_free()
--- bullet.gd
+++ bullet.gd
@@ -1,10 +1,17 @@
 class_name Bullet extends CharacterBody2D
 
 const SPEED: int = 180
+const DAMAGE: int = 1
 
 # Główna funkcja wywoływana w każdej klatce fizyki (60 razy na sekundę domyślnie).
 func _physics_process(delta: float) -> void:
 	move_and_slide()
+	
+	var collision: KinematicCollision2D = get_last_slide_collision()
+	if collision:
+		var collider: Object = collision.get_collider()
+		collider.heal_hurt(-DAMAGE)
+		queue_free()
 	
 func start(start_pos: Vector2, direction: Vector2) -> void:
 	global_position = start_pos
